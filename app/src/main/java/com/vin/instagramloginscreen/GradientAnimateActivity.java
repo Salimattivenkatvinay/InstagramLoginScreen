cc6f6779d633a87e6fc0e0f8ac07a585773307ba
Salimattivenkatvinay <venkatvinay6@gmail.com>
2017-08-20 18:28:11 +0530

    some

--- app/src/main/java/com/vin/instagramloginscreen/GradientAnimateActivity.java
+++ app/src/main/java/com/vin/instagramloginscreen/GradientAnimateActivity.java
@@ -1,5 +1,7 @@
 package com.vin.instagramloginscreen;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class GradientAnimateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gradient_animate);
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.root_layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
     }
 }
--- app/src/main/java/com/vin/instagramloginscreen/LoginActivity.java
+++ app/src/main/java/com/vin/instagramloginscreen/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.vin.instagramloginscreen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,6 +11,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),GradientAnimateActivity.class));
+            }
+        });
+
+
 
     }
 }
